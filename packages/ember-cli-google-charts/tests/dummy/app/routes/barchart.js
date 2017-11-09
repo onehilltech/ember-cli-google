f1fd588bcbf6852635fdb385b02f158b05323662
James H. Hill <hilljh82@gmail.com>
2017-11-09 17:24:41 -0500

    Create test example for bar charts

---
+++ packages/ember-cli-google-charts/tests/dummy/app/routes/barchart.js
@@ -0,0 +1,13 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  model () {
+    return [
+      ['Year', 'Sales', 'Expenses', 'Profit'],
+      ['2014', 1000, 400, 200],
+      ['2015', 1170, 460, 250],
+      ['2016', 660, 1120, 300],
+      ['2017', 1030, 540, 350]
+    ];
+  }
+});
