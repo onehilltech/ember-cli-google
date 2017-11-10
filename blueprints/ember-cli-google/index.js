cafc3f8a57e455ad7a6df07690d6169e883335ac
James H. Hill <hilljh82@gmail.com>
2017-11-10 14:37:22 -0500

    Patched ember-cli-google-charts

--- blueprints/ember-cli-google/index.js
+++ blueprints/ember-cli-google/index.js
@@ -6,7 +6,7 @@ module.exports = {
     return this.addAddonsToProject ({
       packages: [
         {name: 'ember-cli-google-recaptcha', target: '^1.0.0'},
-        {name: 'ember-cli-google-charts', target: '^0.2.0'}
+        {name: 'ember-cli-google-charts', target: '^0.2.1'}
       ]
     });
   }
