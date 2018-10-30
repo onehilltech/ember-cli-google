95fa369747c62dca198eb8c47923db3272ee69c5
James H. Hill <hilljh82@gmail.com>
2018-10-30 13:08:57 -0400

    Imported packages

---
+++ packages/ember-cli-google/blueprints/ember-cli-google/index.js
@@ -0,0 +1,14 @@
+/* eslint-env node */
+module.exports = {
+  normalizeEntityName() {}, // no-op since we're just adding dependencies
+
+  afterInstall () {
+    return this.addAddonsToProject ({
+      packages: [
+        {name: 'ember-cli-google-recaptcha', target: '^1.0.0'},
+        {name: 'ember-cli-google-charts', target: '^0.2.1'},
+        {name: '@onehilltech/ember-cli-google-analytics', target: '^0.3.0'}
+      ]
+    });
+  }
+};
