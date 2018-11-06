20eee736e245c57ad92b9ff151d9f451ec2508a6
James H. Hill <hilljh82@gmail.com>
2018-11-06 17:25:49 -0500

    feat(maps): Added support for heatmap layer

--- packages/ember-cli-google-maps/addon/components/g-heatmap-layer.js
+++ packages/ember-cli-google-maps/addon/components/g-heatmap-layer.js
@@ -1,38 +1,61 @@
 import Component from '@ember/component';
-import layout from '../templates/components/g-heatmap-layer';
+import MapEntity from '../mixins/map-entity';
+
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
-export default Component.extend({
-  layout,
-
-  tagName: 'aside',
-
+export default Component.extend (MapEntity, {
   gMaps: service (),
 
   _heatMap: null,
+
+  /// The data points.
+  data: undefined,
+
+  // Specifies whether heatmaps dissipate on zoom. When dissipating is false
+  // the radius of influence increases with zoom level to ensure that the color
+  // intensity is preserved at any given geographic location. Defaults to false.
+  dissipating: false,
+
+  // The color gradient of the heatmap, specified as an array of CSS color strings.
+  // All CSS3 colors — including RGBA — are supported except for extended named
+  // colors and HSL(A) values.
+  gradient: undefined,
+
+  // The maximum intensity of the heatmap. By default, heatmap colors are dynamically
+  // scaled according to the greatest concentration of points at any particular
+  // pixel on the map. This property allows you to specify a fixed maximum. Setting
+  // the maximum intensity can be helpful when your dataset contains a few outliers
+  // with an unusually high intensity.
+  maxIntensity: undefined,
+
+  // The radius of influence for each data point, in pixels.
+  radius: undefined,
+
+  // The opacity of the heatmap, expressed as a number between 0 and 1.
+  opacity: undefined,
 
   didInsertElement () {
     this._super (...arguments);
 
     this.get ('gMaps').include ('visualization');
-
-    this.get ('gMaps').getInstance ().then (() => {
-      const data = this.get ('data');
-      this._heatMap = new google.maps.visualization.HeatmapLayer ({ data });
-    });
   },
 
-  didUpdateAttrs () {
+  didLoadMap () {
     this._super (...arguments);
 
-    this._heatMap.setMap (this.get ('map'));
+    let options = this.getProperties (['dissipating','gradient','maxIntensity','radius','opacity']);
+    options.data = this.get ('heatMapData');
+
+    this._heatMap = new google.maps.visualization.HeatmapLayer (options);
   },
 
-  ready () {
-
+  getEntity () {
+    return this._heatMap;
   },
 
-  getHeatMap () {
-
-  }
+  heatMapData: computed ('data.[]', function () {
+    let data = this.getWithDefault ('data', []);
+    return data.map (({lat, lng, weight}) => weight ? {location: new google.maps.LatLng (lat, lng), weight } : new google.maps.LatLng (lat, lng));
+  })
 });
--- packages/ember-cli-google-maps/tests/dummy/app/controllers/index.js
+++ packages/ember-cli-google-maps/tests/dummy/app/controllers/index.js
@@ -3,6 +3,23 @@ import Controller from '@ember/controller';
 export default Controller.extend ({
   animation: 'bounce',
 
+  heatMapData: Object.freeze ([
+    {lat: 37.782, lng: -122.447, weight: 25},
+    {lat: 37.782, lng: -122.445},
+    {lat: 37.782, lng: -122.443},
+    {lat: 37.782, lng: -122.441},
+    {lat: 37.782, lng: -122.439},
+    {lat: 37.782, lng: -122.437},
+    {lat: 37.782, lng: -122.435},
+    {lat: 37.782, lng: -122.447},
+    {lat: 37.782, lng: -122.445},
+    {lat: 37.782, lng: -122.443},
+    {lat: 37.782, lng: -122.441},
+    {lat: 37.782, lng: -122.439},
+    {lat: 37.782, lng: -122.437},
+    {lat: 37.782, lng: -122.435}
+  ]),
+
   actions: {
     mapClick (ev) {
       console.log (ev);
