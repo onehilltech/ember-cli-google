f959b0c587d89da6cbd54884548b41cdc96ac2e9
James Hill <james@onehilltech.com>
2020-08-16 09:39:47 -0400

    chore: updating to 3.16

--- packages/ember-cli-google-analytics/addon/initializers/g-analytics-init.js
+++ packages/ember-cli-google-analytics/addon/initializers/g-analytics-init.js
@@ -1,14 +1,16 @@
-import Ember from 'ember';
+import Route from '@ember/routing/route';
+import { assert } from '@ember/debug';
+import { get, getWithDefault } from '@ember/object';
 
 export function initialize (app) {
   // Create the one and only tracker need for the application.
   let ENV = app.resolveRegistration ('config:environment');
-  let trackerId = Ember.get (ENV, 'ember-cli-google.analytics.trackerId');
+  let trackerId = get (ENV, 'ember-cli-google.analytics.trackerId');
 
-  Ember.assert ('Missing ember-cli-google.analytics.trackerId in config/environment.', !!trackerId);
+  assert ('Missing ember-cli-google.analytics.trackerId in config/environment.', !!trackerId);
 
-  let cookieDomain = Ember.getWithDefault (ENV, 'ember-cli-google.analytics.cookieDomain', 'auto');
-  let trackerName = Ember.get (ENV, 'ember-cli-google.analytics.trackerName');
+  let cookieDomain = getWithDefault (ENV, 'ember-cli-google.analytics.cookieDomain', 'auto');
+  let trackerName = get (ENV, 'ember-cli-google.analytics.trackerName');
 
   // We only apply Google Analytics in the production environment. Otherwise, we run
   // the risk of collecting analytics of the application while it is in development,
@@ -26,7 +28,7 @@ export function initialize (app) {
   // We still go through the steps so we ensure the behavior is the same in all
   // environments and we do not run into any surprises.
 
-  Ember.Route.reopen ({
+  Route.reopen ({
     /**
      * Get the url for the current location. This returned url is used to set the current
      * page for the analytics.
