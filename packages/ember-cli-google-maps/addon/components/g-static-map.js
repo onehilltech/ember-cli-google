3a5dceb9bc006ad72caa4f2576efcd8ed8672973
James Hill <james@onehilltech.com>
2021-03-31 20:32:07 -0400

    feat: added Google static map component

--- packages/ember-cli-google-maps/addon/components/g-static-map.js
+++ packages/ember-cli-google-maps/addon/components/g-static-map.js
@@ -1,6 +1,51 @@
-import Component from '@ember/component';
-import layout from '../templates/components/g-static-map';
+import Component from '@glimmer/component';
+import { isPresent } from '@ember/utils';
+import { getOwner } from '@ember/application';
+import { get } from '@ember/object';
 
-export default Component.extend({
-  layout
-});
+export default class GStaticMapComponent extends Component {
+  get options () {
+    let params = [
+      `size=${this.size}`,
+      `maptype=${this.type}`,
+      `zoom=${this.zoom}`,
+      `key=${this.apiKey}`
+    ];
+
+
+    if (isPresent (this.args.center)) {
+      params.push (`center=${encodeURI(this.args.center)}`);
+    }
+
+    return params.join ('&');
+  }
+
+  get size () {
+    return `${this.width}x${this.height}`;
+  }
+
+  get width () {
+    return this.args.width || 600;
+  }
+
+  get height () {
+    return this.args.height || 300;
+  }
+
+  get type () {
+    return this.args.type || 'roadmap';
+  }
+
+  get zoom () {
+    return this.args.zoom || 13;
+  }
+
+  get apiKey () {
+    if (isPresent (this.args.apiKey)) {
+      return this.args.apiKey;
+    }
+
+    const ENV = getOwner (this).resolveRegistration ('config:environment');
+    return get (ENV, 'ember-cli-google.maps.apiKey');
+  }
+}
