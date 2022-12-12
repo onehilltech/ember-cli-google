6cf85809617bc725575c116287b596e7b965231a
James Hill <james@onehilltech.com>
2022-12-12 08:36:04 -0500

    feat: can manually invoke verification on invisible recaptcha

--- packages/ember-cli-google-recaptcha/addon/components/g-recaptcha-invisible.js
+++ packages/ember-cli-google-recaptcha/addon/components/g-recaptcha-invisible.js
@@ -37,10 +37,35 @@ export default class GRecaptchaInvisibleComponent extends CaptchaComponent {
     }
   }
 
+  get listenToSubmit () {
+    const { listenToSubmit = true } = this.args;
+    return listenToSubmit;
+  }
+
   @action
   async submit (ev) {
     // Prevent the default behavior of the form, which is to refresh the page.
     ev.preventDefault ();
+
+    if (this.listenToSubmit) {
+      await this.forceVerify ();
+    }
+  }
+
+  @action
+  async verify (element, [ verify ]) {
+    if (verify) {
+      await this.forceVerify ();
+    }
+  }
+
+  /**
+   * Force the verification of the user
+   */
+  async forceVerify () {
+    // The client wants to to handle the submit event. We need to reset the
+    // control if there is already a response. Otherwise, there is a chance the
+    // response has already been used and it cannot be used again.
 
     if (isPresent (this.response)) {
       await this.reset ();
